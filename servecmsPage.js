8d08be8274873008773a98bc7a2aa75adf4ad539
crisanto tubelleja <crisanto.tubelleja@eacomm.com>
2025-03-28 16:09:59 +0800

    update serving html

--- servecmsPage.js
+++ servecmsPage.js
@@ -8,16 +8,27 @@ const start = () => {
   console.log(path.join(process.cwd(), "/client/dist"));
   fastify.register(fastifyStatic, {
     root: path.join(process.cwd(), "/client/dist"), // This is where your Vue.js build output is located
-    prefix: "/", // Serve files under the root path
+    prefix: "/cms", // Serve files under the root path
   });
   fastify.register(cors, {
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true, // ✅ Allow cookies
   });
-  fastify.get("/", async (request, reply) => {
-    return reply.sendFile("index.html"); // Serve the index.html for SPA routing
+  fastify.route({
+    method: "GET",
+    url: "*",
+    handler: async function (request, reply) {
+      return reply.sendFile("index.html"); // Serve the index.html for SPA routing
+    },
   });
+  //   fastify.get("/*", async (request, reply) => {
+  //     return reply.sendFile("index.html"); // Serve the index.html for SPA routing
+  //   });
+  //   fastify.head("/*", (request, reply) => {
+  //     // Handle HEAD requests, if needed
+  //     return reply.send();
+  //   });
   fastify.listen({ port: 5000 }, (err, address) => {
     if (err) throw err;
     console.log("server is running at port 5000");
