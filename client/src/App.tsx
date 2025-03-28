e07867b23467b2e3365e25cc5e75674418773ddd
crisanto tubelleja <crisanto.tubelleja@eacomm.com>
2025-03-28 15:21:24 +0800

    added new server to serve file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -94,7 +94,7 @@ function AppRoutes() {
 function App() {
   return (
     <>
-      <Router basename="/">
+      <Router basename="/cms">
         <AppRoutes />
       </Router>
       <Toaster_></Toaster_>
