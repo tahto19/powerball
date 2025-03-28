1058a921d93afba0618b696f4cae62591b3b00ad
crisanto tubelleja <crisanto.tubelleja@eacomm.com>
2025-03-28 15:54:22 +0800

    added new server to serve file

--- servecmsPage.js
+++ servecmsPage.js
@@ -9,6 +9,11 @@ const start = () => {
     root: path.join(process.cwd(), "/client/dist"), // This is where your Vue.js build output is located
     prefix: "/", // Serve files under the root path
   });
+  fastify.register(cors, {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true, // ✅ Allow cookies
+  });
   fastify.get("/", async (request, reply) => {
     return reply.sendFile("index.html"); // Serve the index.html for SPA routing
   });
