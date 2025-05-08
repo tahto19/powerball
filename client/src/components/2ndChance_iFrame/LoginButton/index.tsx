ac83a6a8c56f6d144258eb793a85e96cdf35fea7
Justin-Carl <Justin-Carl@users.noreply.github.com>
2025-05-08 13:43:30 +0800

    updated iframes design

--- client/src/components/2ndChance_iFrame/LoginButton/index.tsx
+++ client/src/components/2ndChance_iFrame/LoginButton/index.tsx
@@ -42,7 +42,7 @@ const loginButton = () => {
                 <div className="elementor-element elementor-element-234f1629 elementor-align-right elementor-mobile-align-right elementor-widget elementor-widget-button" data-id="234f1629" data-element_type="widget" data-widget_type="button.default">
                     <div className="elementor-widget-container">
                         <div className="elementor-button-wrapper">
-                            <a id="myButton" className="elementor-button elementor-button-link elementor-size-sm elementor-animation-grow" href="https://18.138.76.86/member-area/">
+                            <a id="myButton" className="elementor-button elementor-button-link elementor-size-sm elementor-animation-grow" href="https://18.138.76.86/member-area/" target="_top" rel="noopener noreferrer">
                                 <span className="elementor-button-content-wrapper">
                                     <span className="elementor-button-icon">
                                         <svg aria-hidden="true" className="e-font-icon-svg e-fas-angle-double-right" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z"></path></svg>			</span>
