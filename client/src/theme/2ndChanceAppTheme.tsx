d34eb69ce4f5166e5ce328e1223ab4fa7ec7965b
Justin-Carl <Justin-Carl@users.noreply.github.com>
2025-05-09 17:22:49 +0800

    Remove darkmode theme

--- client/src/theme/2ndChanceAppTheme.tsx
+++ client/src/theme/2ndChanceAppTheme.tsx
@@ -33,7 +33,7 @@ export default function AppTheme2({
                     colorSchemeSelector: 'data-mui-color-scheme',
                     cssVarPrefix: 'template',
                 },
-                colorSchemes, // Recently added in v6 for building light & dark mode app, see https://mui.com/material-ui/customization/palette/#color-schemes
+                // colorSchemes, // Recently added in v6 for building light & dark mode app, see https://mui.com/material-ui/customization/palette/#color-schemes
                 typography,
                 shadows,
                 shape,
