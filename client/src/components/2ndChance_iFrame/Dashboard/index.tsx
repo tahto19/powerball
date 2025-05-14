87aedd4d71defe543a4babe238f9c0f95fa89b3f
Justin-Carl <Justin-Carl@users.noreply.github.com>
2025-05-14 09:54:28 +0800

    Updated dashboard

--- client/src/components/2ndChance_iFrame/Dashboard/index.tsx
+++ client/src/components/2ndChance_iFrame/Dashboard/index.tsx
@@ -150,7 +150,7 @@ const Dashboard = () => {
                     marginTop: '24px'
                 }}>
                 <MyEntries />
-                <TicketScannedList />
+                <TicketScannedList url="myScan" />
                 <WinnerDetails />
             </Box>
         </>
