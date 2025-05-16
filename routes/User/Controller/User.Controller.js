e847bb9732a8e6f873d3d2d768658f2439312eda
Justin-Carl <Justin-Carl@users.noreply.github.com>
2025-05-16 16:28:50 +0800

    Updated env

--- routes/User/Controller/User.Controller.js
+++ routes/User/Controller/User.Controller.js
@@ -13,7 +13,7 @@ import uc from "../lib/User.class.js";
 import fs from "fs";
 export const getController = async (req, res) => {
   const { offset, limit, sort, filter } = req.body;
-
+  console.log(req.url.includes("api/users/admin"));
   let changeFilter = req.url.includes("api/users/admin")
     ? [...filter, { field: "isAdmin", filter: true, type: "boolean" }]
     : [...filter, { field: "isAdmin", filter: false, type: "boolean" }];
