bb151a1de550232f8d6421d071f468a6c866c956
tahto19 <crisanto.tubelleja@eacomm.com>
2025-06-09 18:09:16 +0800

    scanner check

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,14 +11,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      external: ["html5-qrcode"],
-      output: {
-        globals: {
-          "html5-qrcode": "Html5QrcodeScanner", // or just 'html5Qrcode'
-        },
-      },
-    },
-  },
 });
