88240a84ca4a84703d03ae5570cb2c30688a2dfe
tahto19 <crisanto.tubelleja@eacomm.com>
2025-06-09 17:59:41 +0800

    scanner check

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  ssr: {
+    noExternal: ["html5-qrcode"],
+  },
   build: {
     rollupOptions: {
       output: {
