eb1f2a50b3c5da4c9967557c3f75852a123e276f
tahto19 <crisanto.tubelleja@eacomm.com>
2025-09-10 12:48:41 +0800

    fixed error in export

--- routes/exports/Controller/Export.Controller.js
+++ routes/exports/Controller/Export.Controller.js
@@ -3,11 +3,10 @@ import ec from "../lib/Export.class.js";
 export const exportDataController = async (req, res) => {
   try {
     const { type, dr, filter } = req.body;
-    console.log(type);
-    const dr_ = [moment(dr[0]).startOf("day"), moment(dr[0]).endOf("day")] || [
-      moment().startOf("year"),
-      moment().endOf("year"),
-    ];
+    console.log(dr);
+    const dr_ = dr
+      ? [moment(dr[0]).startOf("day"), moment(dr[0]).endOf("day")]
+      : [moment().startOf("year"), moment().endOf("year")];
     let _r = await ec.getData(type, dr_, filter);
     res.send({ file: _r });
   } catch (err) {
