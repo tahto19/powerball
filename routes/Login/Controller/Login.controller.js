e8e337cb8d41501961cf5e4486196b3db2ccb8eb
tahto19 <crisanto.tubelleja@eacomm.com>
2025-09-15 09:51:16 +0800

    updated

--- routes/Login/Controller/Login.controller.js
+++ routes/Login/Controller/Login.controller.js
@@ -38,7 +38,7 @@ export const LoginController = async (req, res) => {
     console.log("here1");
     let token = await res.jwtSign(changeDetails);
     let encryptToken = await encrpytPassword(token);
-    console.log("here");
+    console.log(encryptToken);
     res
       .setCookie("cookie_pb_1271", encryptToken, {
         domain: "",
