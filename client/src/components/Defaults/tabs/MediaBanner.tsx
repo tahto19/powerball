a7321b8afdd127506e1a22ce512291f3d3d50976
Justin-Carl <Justin-Carl@users.noreply.github.com>
2025-09-17 09:54:15 +0800

    added video style

--- client/src/components/Defaults/tabs/MediaBanner.tsx
+++ client/src/components/Defaults/tabs/MediaBanner.tsx
@@ -129,7 +129,7 @@ const MediaBanner = () => {
                         <CardMedia
                             key={date}
                             component="video"
-                            sx={{ width: "fit-content", height: "181px", border: "3px solid #000", borderRadius: "10px", marginTop: "20px" }}
+                            sx={{ maxWidth: "100%", width: "fit-content", height: "181px", border: "3px solid #000", borderRadius: "10px", marginTop: "20px" }}
                             src={"/media/videos/" + formData.file_location + "?t=" + date}
                             controls
                         />
