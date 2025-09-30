df88f2325eff55eaefbdcabd5be654e73557a358
crisanto tubelleja <141402133+tahto19@users.noreply.github.com>
2025-09-30 10:21:51 +0800

    fix available raflle ticket

--- routes/Ticket/controller/Ticket.controller.js
+++ routes/Ticket/controller/Ticket.controller.js
@@ -333,7 +333,7 @@ export const detailedTicketDetailsHistoryController = async (req, res) => {
     let toReturn = [];
     for (let v of get.list) {
       let temp = {
-        active: v.id,
+        id: v.id,
         active: v.active,
         alphaCode: v.alpha_code,
         availableEntries: v.entries - v.entries_used,
